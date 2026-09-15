8a97e8f241a7bb32cd8b0a2b349c9e2fc4ff670a
ㅤㅤㅤㅤㅤㅤ <100667887+hoansfsfs@users.noreply.github.com>
2026-09-15 22:12:36 +0700

    Create Program.cs

---
+++ Lab1/Program.cs
@@ -0,0 +1,12 @@
+namespace QuanLyThongTinSinhVien
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
