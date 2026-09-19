8b5235cc7bfe7a7ff54f5c09ffadaf65864fdd5a
ㅤㅤㅤㅤㅤㅤ <100667887+hoansfsfs@users.noreply.github.com>
2026-09-19 21:16:59 +0700

    Update NhanVienKinhDoanh.cs

--- BTLOP/16092026/NhanVienKinhDoanh.cs
+++ BTLOP/16092026/NhanVienKinhDoanh.cs
@@ -1,1 +1,44 @@
+using System;
 
+namespace QuanLyNhanVien
+{
+    public class NhanVienKinhDoanh : NhanVien
+    {
+        private double doanhSo;
+
+        public double DoanhSo
+        {
+            get { return doanhSo; }
+            set
+            {
+                if (value >= 0)
+                    doanhSo = value;
+                else
+                    throw new Exception("Doanh số phải >= 0");
+            }
+        }
+
+        public NhanVienKinhDoanh(
+            string maNV,
+            string hoTen,
+            double luongCoBan,
+            double doanhSo)
+            : base(maNV, hoTen, luongCoBan)
+        {
+            DoanhSo = doanhSo;
+        }
+
+        public override double TinhLuong()
+        {
+            return LuongCoBan + DoanhSo * 0.05;
+        }
+
+        public override void HienThiThongTin()
+        {
+            Console.WriteLine("=== NHÂN VIÊN KINH DOANH ===");
+            base.HienThiThongTin();
+            Console.WriteLine($"Doanh số: {DoanhSo:N0}");
+            Console.WriteLine($"Lương thực nhận: {TinhLuong():N0}");
+        }
+    }
+}
