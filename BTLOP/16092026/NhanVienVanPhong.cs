399702f37830ea10af50e581baeb2e7886f4b00a
ㅤㅤㅤㅤㅤㅤ <100667887+hoansfsfs@users.noreply.github.com>
2026-09-19 21:13:09 +0700

    Update NhanVienVanPhong.cs

--- BTLOP/16092026/NhanVienVanPhong.cs
+++ BTLOP/16092026/NhanVienVanPhong.cs
@@ -1,1 +1,44 @@
+using System;
 
+namespace QuanLyNhanVien
+{
+    public class NhanVienVanPhong : NhanVien
+    {
+        private int soNgayLamViec;
+
+        public int SoNgayLamViec
+        {
+            get { return soNgayLamViec; }
+            set
+            {
+                if (value >= 0 && value <= 31)
+                    soNgayLamViec = value;
+                else
+                    throw new Exception("Số ngày làm việc phải từ 0 đến 31");
+            }
+        }
+
+        public NhanVienVanPhong(
+            string maNV,
+            string hoTen,
+            double luongCoBan,
+            int soNgayLamViec)
+            : base(maNV, hoTen, luongCoBan)
+        {
+            SoNgayLamViec = soNgayLamViec;
+        }
+
+        public override double TinhLuong()
+        {
+            return LuongCoBan + SoNgayLamViec * 200000;
+        }
+
+        public override void HienThiThongTin()
+        {
+            Console.WriteLine("=== NHÂN VIÊN VĂN PHÒNG ===");
+            base.HienThiThongTin();
+            Console.WriteLine($"Số ngày làm việc: {SoNgayLamViec}");
+            Console.WriteLine($"Lương thực nhận: {TinhLuong():N0}");
+        }
+    }
+}
